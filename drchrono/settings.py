f9026f03481a1580787c87baa73c8fdc74e18a4d
Talia <taliatrilling@gmail.com>
2017-02-28 10:27:35 -0800

    first commit

--- drchrono/settings.py
+++ drchrono/settings.py
@@ -109,3 +109,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+SOCIAL_AUTH_DRCHRONO_KEY = os.environ['SOCIAL_AUTH_DRCHRONO_KEY']
+SOCIAL_AUTH_DRCHRONO_SECRET = os.environ['SOCIAL_AUTH_DRCHRONO_SECRET']
+SOCIAL_AUTH_DRCHRONO_SCOPE = ['patients:summary:read']
+
+# LOGIN_REDIRECT_URL = 
